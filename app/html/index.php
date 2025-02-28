87cb7826e8b2ea19370d27f5d972cb11d58e482c
Jurij Master <foto888999@gmail.com>
2025-02-28 12:42:55 +0200

    1

--- app/html/index.php
+++ app/html/index.php
@@ -1,6 +1,19 @@
 <?php
+//echo "Hi 555";
 
-echo "Hi 555";
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
+    header('Access-Control-Allow-Headers: token, Content-Type');
+    header('Access-Control-Max-Age: 1728000');
+    header('Content-Length: 0');
+    header('Content-Type: text/plain');
+    die();
+}
+
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+
 
 $servername = "srv1503.hstgr.io";
 $username = "u235058084_sw_user";
